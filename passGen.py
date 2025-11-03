dd511f54917c44761e86c240c2c9cf225367e25b
Mohit <mohitt.tech@gmail.com>
2025-11-03 17:53:30 +0530

    Update passGen.py

--- passGen.py
+++ passGen.py
@@ -3,9 +3,10 @@ import random
 import string
 
 def passGen(length):
-    int(input('Enter password length'))
     chars = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(chars) for _ in range(length))
     return password
 
-# U
+# User input
+length = int(input("Enter password length: "))
+print("Your password is:", passGen(length))
